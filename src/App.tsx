444b9cc76b5ee41b1e2a20918e30c733508d6313
cong-zc <zc961123@outlook.com>
2024-10-08 21:54:22 +0800

    禁用复制

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-// import { invoke } from "@tauri-apps/api/core";
-
 import 首页 from './pages/首页'
+
+import './App.css'
 
 function App() {
 
-  // const 点击触发函数 = () => {
-    
-  //    invoke("打开外部程序")
-    
-  // }
   return (
     <div className="App">
       <首页></首页>
